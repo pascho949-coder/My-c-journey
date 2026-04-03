9ce2f082634d2137d16707fc7fe8a388306a0064
pascho949-coder <pascho949@gmail.com>
2026-04-03 12:17:10 +0200

    array_lesson.c

--- array_lesson.c
+++ array_lesson.c
@@ -8,7 +8,7 @@ int main(void)
     //here looping starts with one.. 
     //for we are checking for the other index.. 
     //greater than index 0.
-    for(int i = 1; i < 4; i++)
+    for(int i = 1; i < 5; i++)
     {
         if(nums[i] > max)
         {
