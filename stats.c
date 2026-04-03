e606fbd52f94bd9fcf25f20bf576618e0c739a1b
pascho949-coder <pascho949@gmail.com>
2026-04-03 12:38:23 +0200

    stat.c

---
+++ stats.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int nums[6] = {22,14,66,31,21,12};
+    
+    //minimum number
+    int mim = nums[0];
+    //total (adding all the numbers of the array)
+    int total = 0;
+
+    //looping for the minimum number and total
+    for(int i = 0; i < 6; i++)
+    {
+        total += nums[i];
+        //checking for least number
+        if (nums[i] < mim)
+        {
+            mim = nums[i];
+        }
+    }
+    //display the output
+    printf("least number = %i\n", mim);
+    printf("total = %i\n", total);
+}
