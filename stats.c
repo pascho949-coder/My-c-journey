8d23e03390c98690a3cf848f2b22294a0e5f2de8
pascho949-coder <pascho949@gmail.com>
2026-04-03 13:30:47 +0200

    stats.c

--- stats.c
+++ stats.c
@@ -8,18 +8,22 @@ int main(void)
     int mim = nums[0];
     //total (adding all the numbers of the array)
     int total = 0;
+    float average = 0;
 
     //looping for the minimum number and total
     for(int i = 0; i < 6; i++)
     {
         total += nums[i];
+        average = (float)total / 6;  
         //checking for least number
         if (nums[i] < mim)
         {
             mim = nums[i];
         }
     }
+
     //display the output
-    printf("least number = %i\n", mim);
+    printf("Aveage = %f\n", average);
     printf("total = %i\n", total);
+    printf("least number = %i\n", mim);
 }
