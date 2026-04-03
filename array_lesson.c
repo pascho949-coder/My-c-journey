2ebe028aa6a1ad6387344ef31d15b6b5b2bdb351
pascho949-coder <pascho949@gmail.com>
2026-04-03 12:12:01 +0200

    array_lesson.c

---
+++ array_lesson.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int nums[5] = {40,12,33,98,70};
+    int max = nums[0];
+
+    //here looping starts with one.. 
+    //for we are checking for the other index.. 
+    //greater than index 0.
+    for(int i = 1; i < 4; i++)
+    {
+        if(nums[i] > max)
+        {
+            //assigning the greater value to max
+            max = nums[i];
+        }
+    }
+    printf("Max = %i\n", max);
+    return 0;
+}
